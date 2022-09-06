59cddde4eb5607270fa12441780f59db06c5da4f
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2022-09-06 16:55:39 +0200

    Hacky preload

--- apps/store-wfe/src/routes/Routes.tsx
+++ apps/store-wfe/src/routes/Routes.tsx
@@ -1,22 +1,42 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Routes as ReactRoutes, Route } from "react-router-dom"
 
 import { MainLayout } from "@app/layouts"
 
 import { HomePage } from "./HomePage"
-import { ProductDetailsPage } from "./ProductDetailsPage"
-import { ProductsPage } from "./ProductsPage"
 
-const RoutesComponent = () => (
-	<ReactRoutes>
-		<Route path="/" element={<MainLayout />}>
-			<Route index element={<HomePage />} />
-			<Route path="products">
-				<Route index element={<ProductsPage />} />
-				<Route path=":id" element={<ProductDetailsPage />} />
-			</Route>
-		</Route>
-	</ReactRoutes>
+const ProductDetailsPage = React.lazy(() =>
+	import("./ProductDetailsPage").then(({ ProductDetailsPage }) => ({ default: ProductDetailsPage }))
 )
+const ProductsPage = React.lazy(() => import("./ProductsPage").then(({ ProductsPage }) => ({ default: ProductsPage })))
+
+let preloaded = false
+const preload = () => {
+	if (preloaded) return
+	preloaded = true
+
+	setTimeout(() => {
+		import("./ProductDetailsPage")
+		import("./ProductsPage")
+	}, 100)
+}
+
+const RoutesComponent = () => {
+	useEffect(preload, [])
+
+	return (
+		<React.Suspense>
+			<ReactRoutes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="products">
+						<Route index element={<ProductsPage />} />
+						<Route path=":id" element={<ProductDetailsPage />} />
+					</Route>
+				</Route>
+			</ReactRoutes>
+		</React.Suspense>
+	)
+}
 
 export const Routes = React.memo(RoutesComponent)
